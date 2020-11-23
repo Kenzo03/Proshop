ff2e45685a6d701f4e6dd2d8a5cba2c2330336c0
Kenzo03 <kevinwijaya480@gmail.com>
2020-11-23 22:09:49 +0700

    add register screen

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -4,12 +4,12 @@ import Product from '../models/productModel.js'
 // @desc    Fetch all products
 // @route   GET /api/products
 // @access  Public
-const getProducts = asyncHandler(async (res) => {
+const getProducts = asyncHandler(async (req, res) => {
     const products = await Product.find({})
     res.json(products)
 })
 
-// @desc    Fetch si ngle product
+// @desc    Fetch single product
 // @route   GET /api/products/:id
 // @access  Public
 const getProductById = asyncHandler(async (req,res) => {
