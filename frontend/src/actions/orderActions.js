db62212ce1b5b08dd641cc070d9ed6ecae7d847b
Kenzo03 <kevinwijaya480@gmail.com>
2020-12-07 21:11:06 +0700

    create order detail screen

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -1,42 +1,76 @@
-import axios from 'axios'
+import Axios from "axios";
 import {
   ORDER_CREATE_FAIL,
   ORDER_CREATE_REQUEST,
   ORDER_CREATE_SUCCESS,
+  ORDER_DETAILS_FAIL,
+  ORDER_DETAILS_REQUEST,
+  ORDER_DETAILS_SUCCESS,
 } from "../constants/orderConstants";
 
 export const createOrder = (order) => async (dispatch, getState) => {
-    try {
-        dispatch({
-            type: ORDER_CREATE_REQUEST
-        })
+  try {
+    dispatch({
+      type: ORDER_CREATE_REQUEST,
+    });
 
-        const { userLogin: { userInfo } } = getState()
+    const {
+      userLogin: { userInfo },
+    } = getState();
 
-        const config = {
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization : `Bearer ${userInfo.token}`
-            }
-        }
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
 
-        const { data } = await Axios.post(`/api/orders`, order, config)
+    const { data } = await Axios.post(`/api/orders`, order, config);
 
-        dispatch({
-            type: ORDER_CREATE_SUCCESS,
-            payload: data
-        })
+    dispatch({
+      type: ORDER_CREATE_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: ORDER_CREATE_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
 
-        dispatch({
-            type: ORDER_CREATE_FAIL,
-            payload: data,
-          })
-    } catch(error) {
-        dispatch({ 
-            type: USER_UPDATE_FAIL,
-            payload: error.response && error.response.data.message 
-                ? error.response.data.message 
-                : error.message
-        })
-    }
-}
+export const getOrderDetails = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: ORDER_DETAILS_REQUEST,
+    });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    const { data } = await Axios.get(`/api/orders/${id}`, config);
+
+    dispatch({
+      type: ORDER_DETAILS_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: ORDER_DETAILS_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
